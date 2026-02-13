028131b227f239a8c139a36f663132dfee80e85f
Himanshu Bhandari <himanshubhandari2191@gmail.com>
2026-02-13 17:41:35 +0530

    front end work on progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,50 @@
-import React, { useState } from 'react';
-import { assets } from './assets/assets';
-import { Routes, Route, useLocation } from 'react-router-dom' // Added Route here
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import ChatBox from './components/ChatBox'
-import Community from './pages/Community' // Ensure this path is correct
-import Credits from './pages/Credits'    // Assuming you have a Credits page
+import Credits from './pages/Credits'
+import Community from './pages/Community'
+import { assets } from './assets/assets'
 import './assets/prism.css'
-import Loading from './pages/Loading';
-import Login from './pages/Login';
-import { useAppContext } from './context/AppContext';
+import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
 
-  const{user} = useAppContext();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const {user, loadingUser} = useAppContext()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
 
-  if(pathname==='/loading') return <Loading/>
-
+  if(pathname === '/loading' || loadingUser) return <Loading />
 
   return (
     <>
+    <Toaster />
+    {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
 
-    {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:hidden' onClick={()=>setIsMenuOpen(true)}/>}
     {user ? (
-      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
-      
-      <div className="flex h-screen w-screen">
-        <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+        <div className="flex h-screen w-screen">
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
           <Routes>
-            <Route path="/" element={<ChatBox />} /> 
-            <Route path="/community" element={<Community />} />
+            <Route path="/" element={<ChatBox />} />
             <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
           </Routes>
-        
+        </div>
       </div>
-    </div>
     ) : (
-
-      <div className="bg-gradient-to-b from-[#242124] to-[#000000] 
-      flex items-center jusitfy-center h-screen w-screen">
-        <Login/>
+      <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+        <Login />
       </div>
     )}
-    
+
+      
     </>
-  )
+  );
 }
 
 export default App
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,52 +1,103 @@
-import { createContext,  useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { dummyChats, dummyUserData } from '../assets/assets';
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+import axios from 'axios';
+import toast from "react-hot-toast";
 
-const AppContext = createContext();
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
-export const AppContextProvider = ({ children })=>{
-    
-    const navigate = useNavigate();
-    const[user, setUser] = useState(null);
-    const[chats, setChats] = useState([]);
-    const[selectedChat, setSelectedChat] = useState(null);
-    const[notifications, setNotifications] = useState([]);
-    const[theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [token, setToken] = useState(localStorage.getItem('token') || null)
+    const [loadingUser, setLoadingUser] = useState(true)
 
     const fetchUser = async () => {
-        setUser(dummyUserData)
+        try {
+            const { data } = await axios.get('/api/user/data', {headers: {Authorization: token}})
+            if(data.success){
+                setUser(data.user)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }finally{
+            setLoadingUser(false)
+        }
+    }
+
+    const createNewChat = async () => {
+        try {
+            if(!user) return toast('Login to create a new chat')
+                navigate('/')
+            await axios.get('/api/chat/create', {headers: {Authorization: token}})
+            await fetchUsersChats()
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
 
     const fetchUsersChats = async () => {
-        setChats(dummyChats);
-        setSelectedChat(dummyChats[0]);
+        try {
+            const {data} = await axios.get('/api/chat/get', {headers: {Authorization: token}})
+            if(data.success){
+                setChats(data.chats)
+                //If user has no chats create one
+                if(data.chats.length === 0){
+                    await createNewChat();
+                    return fetchUsersChats()
+                }else{
+                    setSelectedChat(data.chats[0])
+                }
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
 
     useEffect(() => {
-        if(theme === 'dark'){
+        if (theme === 'dark') {
             document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
+
+    useEffect(() => {
+        if (user) {
+            fetchUsersChats()
+        }
+        else {
+            setChats([])
+            setSelectedChat(null)
+        }
+    }, [user])
+
+    useEffect(() => {
+        if(token){
+            fetchUser()
         }else{
-            document.documentElement.classList.remove('dark');
+            setUser(null)
+            setLoadingUser(false)
         }
-        localStorage.setItem('theme', theme);
-    }, [theme]);
+    }, [token])
 
-    useEffect(() => {
-        if(user){
-            fetchUsersChats();
-        }
-        else{
-            setChats([]);
-            setSelectedChat(null);
-        }
-    }, [user]);
+    const value = {
+        navigate, user, setUser, fetchUser, chats, setChats, selectedChat, setSelectedChat, theme, setTheme, createNewChat, loadingUser, fetchUsersChats, token, setToken, axios
+    }
 
-    useEffect(() => {
-        fetchUser();
-    }, [])
-
-    const value = {navigate, user, setUser, chats, setChats, selectedChat, setSelectedChat, notifications, setNotifications, theme, setTheme};
-    
     return (
         <AppContext.Provider value={value}>
             {children}
@@ -54,4 +105,4 @@ export const AppContextProvider = ({ children })=>{
     )
 }
 
-export const useAppContext = () => useContext(AppContext);
+export const useAppContext = () => useContext(AppContext)
